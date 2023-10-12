34bfb4e69c5daf53d4b3731f08367d7a4e4abfe8
7Rena7 <renacorbe@gmail.com>
2023-10-12 13:28:35 -0300

    first commit

---
+++ src/app/shared/breadcrumps/breadcrumps.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumps',
+  templateUrl: './breadcrumps.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumpsComponent {
+
+}
